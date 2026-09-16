c345f29a21528e3230fa6b134e3b67848c3c487a
Henrique-c2 <hibc22@gmail.com>
2026-09-16 15:51:15 -0300

    refactor: arrumando a main e passando todos os códigos green.

--- main.py
+++ main.py
@@ -18,30 +18,65 @@ def conectar_banco():
     )
     return conn
 
-@app.route("/imoveis", methods=["GET"])
-def listar_imoveis():
-    conn = conectar_banco()
-    cursor = conn.cursor()
-
+def buscar_todos(cursor):
     cursor.execute( 
     "SELECT id,logradouro,tipo_logradouro,bairro,cidade,cep,tipo,valor,data_aquisicao FROM imoveis"
     )
-    imoveis = cursor.fetchall()
+    return cursor.fetchall()
+
+def buscar_por_tipo(cursor, tipo):
+    cursor.execute(
+    "SELECT id,logradouro,tipo_logradouro,bairro,cidade,cep,tipo,valor,data_aquisicao FROM imoveis WHERE tipo = %s",
+    (tipo,)
+    )
+    return cursor.fetchall()
+
+def buscar_por_cidade(cursor, cidade):
+    cursor.execute(
+    "SELECT id,logradouro,tipo_logradouro,bairro,cidade,cep,tipo,valor,data_aquisicao FROM imoveis WHERE cidade = %s",
+    (cidade,)
+    )
+    return cursor.fetchall()
+
+@app.route("/imoveis", methods=["GET"])
+def listar_imoveis():
+    tipo = request.args.get("tipo")
+    cidade = request.args.get("cidade")
+
+    conn = conectar_banco()
+    cursor = conn.cursor()
+
+    if tipo:
+        imoveis = buscar_por_tipo(cursor, tipo)
+    elif cidade:
+        imoveis = buscar_por_cidade(cursor, cidade)
+    else:
+        imoveis = buscar_todos(cursor)
 
     cursor.close()
     conn.close()
 
+    if tipo and len(imoveis) == 0:
+        return jsonify({
+            "erro": "Não foi encontrado este tipo de imovel."
+        }), 404
+
+    if cidade and len(imoveis) == 0:
+        return jsonify({
+            "erro": "A cidade não foi encontrada na lista de imóveis."
+        }), 404
+
     imoveis_formatados = [
     {
-        "id": imoveis[0],
-        "logradouro": imoveis[1],
-        "tipo_logradouro": imoveis[2],
-        "bairro": imoveis[3],
-        "cidade": imoveis[4],
-        "cep": imoveis[5],
-        "tipo": imoveis[6],
-        "valor": imoveis[7],
-        "data_aquisicao": imoveis[8]
+        "id": imovel[0],
+        "logradouro": imovel[1],
+        "tipo_logradouro": imovel[2],
+        "bairro": imovel[3],
+        "cidade": imovel[4],
+        "cep": imovel[5],
+        "tipo": imovel[6],
+        "valor": imovel[7],
+        "data_aquisicao": imovel[8]
     }
     for imovel in imoveis
     ]
@@ -98,13 +133,13 @@ def adicionar_imovel():
         (novo["logradouro"],novo["tipo_logradouro"],novo["bairro"],novo["cidade"],novo["cep"],novo["tipo"],novo["valor"],novo["data_aquisicao"])
     )
 
-    conn.commit
+    conn.commit()
     novo_id = cursor.lastrowid
 
     cursor.close()
     conn.close()
 
-    return jsonify({"id":novo_id}),200
+    return jsonify({"id":novo_id}),201
 
 
 @app.route("/imoveis/<int:id>", methods=["PUT"])
@@ -176,97 +211,9 @@ def remover_imovel(id):
 
     if linhas == 0:
         return jsonify({
-            "erro":"Imovel não encontrado."
-        })
+            "erro":"Imovel não encontrado"
+        }), 404
 
     return jsonify({
         "mensagem": "Imovel deletado com sucesso"
     }),200
-
-@app.route("/imoveis", methods=["GET"])
-def listar_imovel_tipo():
-    tipo = request.args.get("tipo")
-
-    conn = conectar_banco()
-    cursor = conn.cursor()
-
-    if tipo:
-        cursor.execute(
-            "SELECT id,logradouro,tipo_logradouro,bairro,cidade,cep,tipo,valor,data_aquisicao FROM imoveis WHERE tipo = %s",
-            (tipo,)
-        )
-    else:
-        cursor.execute(
-            "SELECT id,logradouro,tipo_logradouro,bairro,cidade,cep,tipo,valor,data_aquisicao FROM imoveis"
-        )
-
-    imoveis = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    if tipo and len(imoveis) == 0:
-        return jsonify({
-            "erro": "Não foi encontrado este tipo de imovel."
-        }), 404
-
-    imoveis_formatados = [
-        {
-            "id": imovel[0],
-            "logradouro": imovel[1],
-            "tipo_logradouro": imovel[2],
-            "bairro": imovel[3],
-            "cidade": imovel[4],
-            "cep": imovel[5],
-            "tipo": imovel[6],
-            "valor": imovel[7],
-            "data_aquisicao": imovel[8]
-        }
-        for imovel in imoveis
-    ]
-
-    return jsonify(imoveis_formatados), 200
-
-@app.route("/imoveis",methods = ["GET"])
-def listar_imovel_cidade():
-    cidade = request.args.get("cidade")
-
-    conn = conectar_banco()
-    cursor = conn.cursor()
-
-    if cidade:
-        cursor.execute(
-            "SELECT id,logradouro,tipo_logradouro,bairro,cidade,cep,tipo,valor,data_aquisicao FROM imoveis WHERE cidade = %s",
-            (cidade,)
-        )
-    else:
-        cursor.execute(
-            "SELECT id,logradouro,tipo_logradouro,bairro,cidade,cep,tipo,valor,data_aquisicao FROM imoveis"
-        )
-
-    imoveis = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    if cidade and len(imoveis) == 0:
-        return jsonify({
-            "erro": "Não foi encontrado este cidade de imovel."
-        }), 404
-
-    imoveis_formatados = [
-        {
-            "id": imovel[0],
-            "logradouro": imovel[1],
-            "tipo_logradouro": imovel[2],
-            "bairro": imovel[3],
-            "cidade": imovel[4],
-            "cep": imovel[5],
-            "tipo": imovel[6],
-            "valor": imovel[7],
-            "data_aquisicao": imovel[8]
-        }
-        for imovel in imoveis
-    ]
-
-    return jsonify(imoveis_formatados), 200
